487f5d2d35de38d97b234548e48e6e1a96ff2642
charan_kanduri <charan2000kanduri@gmail.com>
2021-08-06 21:39:05 +0530

    [CHARAN]UC-9

--- EmployeePayRoll/SQLQuery1.sql
+++ EmployeePayRoll/SQLQuery1.sql
@@ -59,5 +59,20 @@ where EmployeeName='Rujula';
 Update employee_payroll 
 set EmployeePhoneNumber='8715605050',Address='Bareilly,UP'
 where EmployeeName='Gayatri KG';
+------- UC 9: Rename Salary to Basic Pay and Add Deduction,Taxable pay, Income Pay , Netpay -------
+
+EXEC sp_rename 'employee_payroll.Deduction' , 'Deduce', 'COLUMN'
+Alter table employee_payroll
+add Deduce float,TaxablePay float, IncomeTax float,NetPay float;
+Update employee_payroll 
+set Deduction=1000
+where Gender='F';
+Update employee_payroll 
+set Deduction=2000
+where Gender='M';
+update employee_payroll
+set NetPay=(BasicPay - Deduction)
+update employee_payroll
+set NetPay=0,Deduction=0
 
 select * from employee_payroll;
